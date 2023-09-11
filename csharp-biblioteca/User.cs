aef59a6122fa15ffb218fde3c637afa18de724e6
giovannitamburini <giovatambu@gmail.com>
2023-09-11 14:30:06 +0200

    Aggiungere i file di progetto.

---
+++ csharp-biblioteca/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca
+{
+    public class User
+    {
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string TelephoneNumber { get; set; }
+
+        // COSTRUTTORE
+
+        public User(string surname, string name, string email, string password, string telephoneNumber)
+        {
+            this.Surname = surname;
+            this.Name = name;
+            this.Email = email;
+            this.Password = password;
+            this.TelephoneNumber = telephoneNumber;
+        } 
+
+    }
+}
